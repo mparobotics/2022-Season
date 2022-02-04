8b59fc472da76fd557122acea37f899e9f13080d
wg7321 <85994145+wg7321@users.noreply.github.com>
2022-02-04 15:49:36 -0600

    Correcting Git Commit Error

--- Rapid React/src/main/java/frc/robot/subsystems/ElevatorSub.java
+++ Rapid React/src/main/java/frc/robot/subsystems/ElevatorSub.java
@@ -3,115 +3,45 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import frc.robot.Constants.DriveConstants;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+//import edu.wpi.first.wpilibj.XboxController;
+//import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
+//import frc.robot.Constants.IntakeConstants;
 
-public class DriveSubsystem extends SubsystemBase {
-  /** Defines and initializes motors using IDs from constants */
-  private final WPI_TalonFX falconBL = new WPI_TalonFX(DriveConstants.FALCON_BL_ID); 
-  private final WPI_TalonFX falconFL = new WPI_TalonFX(DriveConstants.FALCON_FL_ID); 
-  private final WPI_TalonFX falconBR = new WPI_TalonFX(DriveConstants.FALCON_BR_ID); 
-  private final WPI_TalonFX falconFR = new WPI_TalonFX(DriveConstants.FALCON_FR_ID); 
+public class ElevatorSub extends SubsystemBase {
+  /** Creates a new Intake. */
+  static WPI_TalonFX frontElev;
+  static WPI_TalonFX backElev;
+  MotorControllerGroup elevatorMotors;
 
-  private final TalonFXConfiguration fxConfig = new TalonFXConfiguration();
+  public ElevatorSub() {
+    frontElev = new WPI_TalonFX(ElevatorConstants.FRONT_ELEVATOR_ID);
+    frontElev.setInverted(false);
+    backElev = new WPI_TalonFX(ElevatorConstants.BACK_ELEVATOR_ID);
+    backElev.setInverted(true);
 
-  /** makes speed control groups, now called motor control groups. yay changing wording
-   * for no good reason wasting an hour of my time, yay! */
-  private final MotorControllerGroup MCG_R = new MotorControllerGroup(falconFR, falconBR); 
-  private final MotorControllerGroup MCG_L = new MotorControllerGroup(falconFL, falconBL); 
-
-  private final DifferentialDrive drive = new DifferentialDrive(MCG_L, MCG_R); //todo check right side inversion
-
-  private double HeadingError; // calculates sensor error for driving straight
-  private double integral;
-
-  public DriveSubsystem() {
-    
-}
-
-  public void setCoast() {
-    //setting coast or brake mode, can also be done in Phoenix tuner
-    falconFR.setNeutralMode(NeutralMode.Coast);
-    falconFL.setNeutralMode(NeutralMode.Coast);
-    falconBR.setNeutralMode(NeutralMode.Coast);
-    falconBL.setNeutralMode(NeutralMode.Coast);
-}
-  public void setBrake() {
-    //setting coast or brake mode, can also be done in Phoenix tuner
-    falconFR.setNeutralMode(NeutralMode.Brake);
-    falconFL.setNeutralMode(NeutralMode.Brake);
-    falconBR.setNeutralMode(NeutralMode.Brake);
-    falconBL.setNeutralMode(NeutralMode.Brake);
-}
+    //elevatorMotors = new MotorControllerGroup(frontElev, backElev);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-  //calculates stuff so we can drive straight
-  private double driveTrainP() {
-    HeadingError = falconFL.getSelectedSensorPosition() - falconFR.getSelectedSensorPosition();
-    //integral += error*.02;
-    return DriveConstants.DRIVE_P * HeadingError;
-  }
-  //drives straight
-  public void driveStraight(double xSpeed) {
-    drive.arcadeDrive(xSpeed, /* -? */driveTrainP());
-  }
-  /**
-   * sets the speed of the drive train with arcade controls ask Mikey what it does? If you see this remind me to ask them.
-   * @param xSpeed
-   * @param zRotation
-   */
-  
-   //arcade drive, yay! discussion should we do tank this year? discussion we need to have
-   public void setDriveSpeed_Arcade(double xSpeed, double zRotation) {
-    if (zRotation == 0 ) //todo test variable thresholds: if (zRotation > -.1 && zRotation < .1)
-      driveStraight(xSpeed);
-    drive.arcadeDrive(xSpeed, zRotation);
+
+  public static void ElevateBall(double speed)
+  {
+    frontElev.set(speed);
+    backElev.set(speed);
   }
 
-  /**
-   * set the speed of the drive train with tank controls (WIP) still not sure what this does -ian
-   * @param lSpeed
-   * @param rSpeed
-   */
-  //its tank drive! in case we want to use tank drive here is is! how fun! how riveting! is that how i speel riveting?
-   public void setDriveSpeed_Tank(double lSpeed, double rSpeed) {
-    drive.tankDrive(lSpeed, rSpeed);
-  }
-
-    /**
-   * stops the drive train
-   */
-  public void stopRobot() {
-    falconFR.set(ControlMode.PercentOutput, 0);
-    falconFL.set(ControlMode.PercentOutput, 0);
-  }
-
-    /**
-   * prints encoder values to the smart dashboard
-   */
-  public void printEncoderValues() {
-    SmartDashboard.putNumber("FR pos", falconFR.getSelectedSensorPosition());
-    SmartDashboard.putNumber("BR pos", falconBR.getSelectedSensorPosition());
-    SmartDashboard.putNumber("FL pos", falconFL.getSelectedSensorPosition());
-    SmartDashboard.putNumber("BL pos", falconBL.getSelectedSensorPosition());
-  }
-
-  public void encoderReset() {
-    falconFR.setSelectedSensorPosition(0);
-    falconFL.setSelectedSensorPosition(0);
-    falconBR.setSelectedSensorPosition(0);
-    falconBL.setSelectedSensorPosition(0);
+  public void stop()
+  {
+    frontElev.set(0);
+    backElev.set(0);
   }
 }
